8b9c6efb54794adaee58b9475e7a94a67394e8e2
Jonathan Koban <jon.koban@outlook.com>
2022-04-06 13:28:57 -0400

    commit tree c039e6871e6964959dd1dbdb5a481171534311c4

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(array) {
+    return (array.slice(0,2));
+}
+
+function returnLastTwoDrivers (array) {
+    return (array.slice(-2))
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function actualFareMultiplier(multiplier) {
+        return (integer * multiplier)
+    }
+}
+
+function fareDoubler (number) {
+    return (number * 2);
+}
+
+function fareTripler(number) {
+    return number * 3
+}
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+}
+
+function selectDifferentDrivers(drivers, returnLastTwoDrivers) {
+    return returnLastTwoDrivers(drivers);
+}
